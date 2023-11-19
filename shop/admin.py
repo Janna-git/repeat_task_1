0f93a3b86272a75a7b4d3c24f31c678c9d834a01
Janna-git <zhannat.shayakunova@mail.ru>
2023-11-19 20:07:22 +0600

    add views.py, urls.py, templates

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item, Purchase
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+
+admin.site.register(Item, ItemAdmin)
+
+
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'age', 'item', 'date_purchase']
+
+
+admin.site.register(Purchase, PurchaseAdmin)
--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('shop/greetings/', views.greetings, name='greetings'),
+    path('shop/', views.list_item, name='item_list'),
+    path('<int:item_id>', views.detail_item, name='detail_item'),
 ]
